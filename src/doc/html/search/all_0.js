a871f4d99ba7d6cdcda6763bd359c432032a63e6
leonid <leonadkr@gmail.com>
2025-05-29 15:31:43 +1000

    delete pregenerated documentations

--- src/doc/html/search/all_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['_5fdictclienterror_0',['_DictClientError',['../glibdictclient_8h.html#a5d26187426b08c5f5f11af2f33e420f0',1,'glibdictclient.h']]]
-];
--- src/doc/html/search/typedefs_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['dictclienterror_0',['DictClientError',['../glibdictclient_8h.html#a575536a01f7305a42933517053bc4d20',1,'glibdictclient.h']]]
-];
